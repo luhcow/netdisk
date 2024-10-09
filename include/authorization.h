bafc74433a32680634f9bd7a9639b55cd89a99f0
shimmer <wxiangyn@outlook.com>
2024-10-09 22:44:03 +0800

    1009

--- include/authorization.h
+++ include/authorization.h
@@ -1,8 +1,7 @@
+#include <l8w8jwt/claim.h>
+#include <l8w8jwt/decode.h>
 #include <stdbool.h>
 #include <time.h>
-
-#include "l8w8jwt/claim.h"
-#include "l8w8jwt/decode.h"
 
 bool decode_jwt(char JWT[], struct l8w8jwt_claim **out_claims,
                 int *out_calims_length);
