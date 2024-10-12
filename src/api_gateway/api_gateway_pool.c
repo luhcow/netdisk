86fee42f3c4010a6a1b330fc85e4ec9c505a8a74
shimmer <wxiangyn@outlook.com>
2024-10-12 14:23:32 +0000

    注册信号处理函数

--- src/api_gateway/api_gateway_pool.c
+++ src/api_gateway/api_gateway_pool.c
@@ -1,30 +1,37 @@
+#include <signal.h>
+
 #include "api_gateway.h"
 #include "pool.h"
 
 BlockQ pool;
+pthread_t pool_thread;
+
+int server_sock = -1;
+
+void handler(int sign);
+
 int main(int argc, char* argv[]) {
+    // 注册信号
+    signal(SIGINT, handler);
+
     // 建线程池
-    pthread_t pool_thread;
     pthread_create(&pool_thread, NULL, gateway_pool_build, &pool);
 
     // 接受连接
-    int server_sock = -1;
-
     unsigned short int port = 0;
     if (argc != 1)
         port = atoi(argv[1]);
-    int client_sock = -1;
-    struct sockaddr_in client_name;
-    int client_name_len = sizeof(client_name);
 
     // 在对应端口建立 httpd 服务
     server_sock = startup(&port);
     fprintf(stderr, "httpd running on port %d\n", port);
 
     // 生产数据
+    struct sockaddr_in client_name;
+    int client_name_len = sizeof(client_name);
     while (1) {
-        /*套接字收到客户端连接请求*/
-        client_sock =
+        // 套接字收到客户端连接请求
+        int client_sock =
                 accept(server_sock, (struct sockaddr*)&client_name,
                        &client_name_len);
         if (client_sock == -1)
@@ -38,4 +45,8 @@ int main(int argc, char* argv[]) {
     pthread_join(pool_thread, NULL);
     exit(0);
     return 0;
+}
+void handler(int sign) {
+    close(server_sock);
+    exit(0);
 }
