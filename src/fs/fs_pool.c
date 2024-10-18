2f1a3b93305d0992c3cdd91ef3df0b87294eca58
Oryza <Oryza@artificer.com>
2024-10-18 21:59:38 +0800

    123

--- src/fs/fs_pool.c
+++ src/fs/fs_pool.c
@@ -1,3 +1,5 @@
+#define _POSIX_C_SOURCE 200809L
+
 #include <json.h>
 #include <mysql/mysql.h>
 #include <pthread.h>
@@ -5,25 +7,27 @@
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "fs.h"
 #include "nd_mysql.h"
 #include "pool.h"
+#include "rabbitmq_p.h"
 
-struct pool_t *pool;
+struct pool_t* pool;
 
-extern char rabbitmq_hostname[16];
+extern char* rabbitmq_hostname;
 extern int rabbitmq_port;
-extern char rabbitmq_vhost[16];
+extern char* rabbitmq_vhost;
 extern amqp_channel_t rabbitmq_channel;
-extern char rabbitmq_exchange[16];
-extern char rabbitmq_exchange_type[16];
+extern char* rabbitmq_exchange;
+extern char* rabbitmq_exchange_type;
 
 void capture_signal(int sign);
 void exit_processing(void);
 void conf_read(void);
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     // if (daemon(0, 0) == -1) {
     //     // Error handling
     // }
@@ -41,8 +45,7 @@ int main(int argc, char *argv[]) {
     // 建线程池
     pool = pool_create(fs_run, false, 2);
     pool->build(pool);
-    // 线程池退出
-    pool->cancel(pool);
+    while (1) sleep(100);
 
     exit(0);
     return 0;
@@ -63,33 +66,25 @@ void exit_processing(void) {
 }
 
 void conf_read(void) {
-    FILE *conf = fopen("api_gateway.json", "r");
+    FILE* conf = fopen("api_gateway.json", "r");
     fseek(conf, 0, SEEK_END);
     long conf_len = ftell(conf);
     fseek(conf, 0, SEEK_SET);
     char buf[conf_len + 1];
     fread(buf, 1, conf_len, conf);
 
-    struct json_object *jobj = json_tokener_parse(buf);
+    struct json_object* jobj = json_tokener_parse(buf);
 
-    strncpy(rabbitmq_hostname,
-            json_object_get_string(json_object_object_get(
-                    jobj, "rabbitmq_hostname")),
-            16);
+    rabbitmq_hostname = strdup(json_object_get_string(
+            json_object_object_get(jobj, "rabbitmq_hostname")));
     rabbitmq_port = json_object_get_int(
             json_object_object_get(jobj, "rabbitmq_port"));
-    strncpy(rabbitmq_vhost,
-            json_object_get_string(
-                    json_object_object_get(jobj, "rabbitmq_vhost")),
-            16);
+    rabbitmq_vhost = strdup(json_object_get_string(
+            json_object_object_get(jobj, "rabbitmq_vhost")));
     rabbitmq_channel = json_object_get_int(
             json_object_object_get(jobj, "rabbitmq_channel"));
-    strncpy(rabbitmq_exchange,
-            json_object_get_string(json_object_object_get(
-                    jobj, "rabbitmq_exchange")),
-            16);
-    strncpy(rabbitmq_exchange_type,
-            json_object_get_string(json_object_object_get(
-                    jobj, "rabbitmq_exchange_type")),
-            16);
+    rabbitmq_exchange = strdup(json_object_get_string(
+            json_object_object_get(jobj, "rabbitmq_exchange")));
+    rabbitmq_exchange_type = strdup(json_object_get_string(
+            json_object_object_get(jobj, "rabbitmq_exchange_type")));
 }
